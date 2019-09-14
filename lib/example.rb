7ba3042350e3c0405455dc03bc26b2ed4ac115ff
Meagan Utsey <meagan.utsey@gmail.com>
2019-09-14 15:40:03 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -10,16 +10,7 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+end 
+
+say_hello_world_ten_times
+
